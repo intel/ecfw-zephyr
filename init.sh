6523b3df5c234cd7cee058962674fc8d4b7d88a4
Maximilian Brune <maximilian.brune@9elements.com>
2022-08-25 16:13:30 +0200

    Fix init.sh script

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export EVERGLADES_SPI_GEN=./SPI_image_gen/everglades_spi_gen_RomE
-export            SPI_GEN=./SPI_image_gen/everglades_spi_gen_RomE
+export EVERGLADES_SPI_GEN="$PWD/tools/everglades_spi_gen/everglades_spi_gen_RomE"
+export            SPI_GEN="$PWD/tools/everglades_spi_gen/everglades_spi_gen_RomE"
 . ../ecfwwork/zephyr_fork/zephyr-env.sh
 . ../../zephyr-virtualenv/bin/activate

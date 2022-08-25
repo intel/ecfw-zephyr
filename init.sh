d06763e6a66d708c45a2272c1cdc98c5d60196de
Maximilian Brune <maximilian.brune@9elements.com>
2022-08-25 14:12:13 +0200

    Add init script to get started faster
    It's quite tedious to always have to do setup work,
    just because one opens a new terminal and therefore looses
    all enviroment variables. Therefore this script.
    Just apply this script: source ./init.sh
    and one should be ready to work instantly.
    It assumes either a python virtualenv folder is setup, which
    this script sources or python package are already installed
    system wide.

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export EVERGLADES_SPI_GEN=./SPI_image_gen/everglades_spi_gen_RomE
+export            SPI_GEN=./SPI_image_gen/everglades_spi_gen_RomE
+. ../ecfwwork/zephyr_fork/zephyr-env.sh
+. ../../zephyr-virtualenv/bin/activate

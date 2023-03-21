c7807574311b3bd6a926b37c7a5d2952a18bb7e9
Jose Alberto Meza <jose.a.meza.arellano@intel.com>
2023-03-20 19:29:52 -0700

    app: Remove Adjust Boot delay
    Remove unnecessary delay that affects SUS_ACK on G3 for all
    boards.
    
    Signed-off-by: Jose Alberto Meza <jose.a.meza.arellano@intel.com>

--- app/app.c
+++ app/app.c
@@ -23,13 +23,7 @@ int main(void)
 	int ret;
 
 	/* Delayed start for debug */
-	k_sleep(K_SECONDS(CONFIG_EC_DELAYED_BOOT));
-
-	/* In platform N-1 rework doesn't route SMC_RST to MECC card, causing
-	 * I2C glitches among other issues, add a delay to get postcodes
-	 * until HW WA is possible.
-	 */
-	k_msleep(100);
+	k_msleep(CONFIG_EC_DELAYED_BOOT);
 
 	LOG_INF("EC FW Zephyr %p %s", k_current_get(), CONFIG_BOARD);
 

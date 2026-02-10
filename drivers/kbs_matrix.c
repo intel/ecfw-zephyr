4ca4a4946e25b471f1f9f18ba8ec06719749db7e
Lalatendu Satpathy <lalatendu.satpathy@intel.com>
2026-02-10 11:55:38 -0800

    drivers: kbs: correct bit masking order in typematic delay calculation
    fix: correct bit field extraction in kbs_write_typematic
    
    Fixed incorrect expression in typematic_delay_idx calculation where
    bitwise operations were applied in wrong order. The original code
    shifted before masking, which doesn't correctly extract bits [6:5].
    
    Signed-off-by: Lalatendu Satpathy <lalatendu.satpathy@intel.com>
    Signed-off-by: Jose Alberto Meza <jose.a.meza.arellano@intel.com>

--- drivers/kbs_matrix.c
+++ drivers/kbs_matrix.c
@@ -382,14 +382,25 @@ int kbs_matrix_init(kbs_matrix_callback callback, uint8_t *initial_set)
 	return 0;
 }
 
+/**
+ * Set typematic rate and delay according to the PS/2 keyboard 0xF3 command.
+ *
+ * The 0xF3 command (Set Typematic Rate/Delay) is sent by the host to the keyboard,
+ * followed by a data byte encoding the repeat rate and delay:
+ *   - Bits 0-4: Typematic rate (lower value = faster repeat)
+ *   - Bits 5-6: Typematic delay (00=250ms, 01=500ms, 10=750ms, 11=1000ms)
+ *   - Bit 7: Unused
+ * This function extracts the rate and delay from the data byte and updates the
+ * typematic settings accordingly. The timer is stopped before updating.
+ */
 void kbs_write_typematic(uint8_t data)
 {
 	/* Cancel typematic timer before attempting to change settings */
 	k_timer_stop(&typematic_timer);
 
-	typematic_period_idx = data  & TYPEMATIC_RATE_MASK;
+	typematic_period_idx = data & TYPEMATIC_RATE_MASK;
 	typematic_delay_idx =
-		(data >> TYPEMATIC_DELAY_POS) & TYPEMATIC_DELAY_MASK;
+		(data & TYPEMATIC_DELAY_MASK) >> TYPEMATIC_DELAY_POS;
 }
 
 void kbs_keyboard_enable(void)
